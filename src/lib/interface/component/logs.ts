7d3f2ff8f51788be62c0bd00ceac36c8948fbb9b
wss-git <wssgryx@163.com>
2021-06-03 17:24:00 +0800

    feat: integrated nas/sync/invoke/metrics

---
+++ src/lib/interface/component/logs.ts
@@ -0,0 +1,8 @@
+import { LogConfig } from '../sls';
+
+export interface LogsProps {
+  region: string;
+  logConfig: LogConfig;
+  topic: string;
+  query: string;
+}
--- src/lib/interface/sls.ts
+++ src/lib/interface/sls.ts
@@ -2,3 +2,7 @@ export interface LogConfig {
   project: string;
   logstore: string;
 }
+
+export function isLogConfig(args: any): args is LogConfig {
+  return args && args.project && args.logstore;
+}
