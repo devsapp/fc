a821292153fc84170493e11caabf43dda3c32900
wss-git <wssgryx@163.com>
2021-10-26 09:55:56 +0800

    style: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,13 @@ module.exports = {
     node: true,
     jest: true,
   },
+  rules: {
+    'max-len': 'off',
+    'no-param-reassign': 'off',
+    'no-await-in-loop': 'off',
+    'require-atomic-updates': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'no-console': 'off',
+  },
 };
