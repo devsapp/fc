75903015defc70dfd87cd36c7f8ff43e65004f3a
xsahxl <xsahxl@126.com>
2021-12-24 16:09:08 +0800

    test: 💍 logger 升级

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,8 +1,28 @@
-
 import { Logger } from '@serverless-devs/core';
+import os from 'os';
+import path from 'path';
+import rimraf from 'rimraf';
+import { spawnSync } from 'child_process';
 
 export const CONTEXT = 'FC';
 
 const logger = new Logger(CONTEXT);
+
+if (typeof logger.task !== 'function') {
+  const cachePath = path.join(os.homedir(), '.s', 'cache');
+  rimraf.sync(path.join(cachePath));
+
+  try {
+    spawnSync('npm install @serverless-devs/s -g', { shell: true });
+  } catch (error) {
+    try {
+      spawnSync('yarn global add @serverless-devs/s', { shell: true });
+    } catch (error) {
+      logger.warn(
+        "It is detected that your version is an old version, you can run 'npm install @serverless-devs/s -g' to update",
+      );
+    }
+  }
+}
 
 export default logger;
