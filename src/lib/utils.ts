d436fea007dd126af35e03d1f78b6b83888caee6
wss-git <wssgryx@163.com>
2022-03-04 16:09:23 +0800

    fix: nas auto get config from remote

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,10 +31,6 @@ export function isAutoConfig(config: any): boolean {
   return config === 'auto' || config === 'Auto';
 }
 
-export function genServiceStateID(accountID, region, serviceName): string {
-  return `${accountID}-${region}-${serviceName}`;
-}
-
 export function getFcNames(argsParse, inputsProps) {
   if (argsParse?.region) {
     return {
