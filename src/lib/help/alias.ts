f0614eaef14b7074dfd1fff31dfee912cc5bf6d6
pichunying <2339348937@qq.com>
2022-07-02 16:59:46 +0800

    fix: add doc about 'rollback'

--- src/lib/help/alias.ts
+++ src/lib/help/alias.ts
@@ -31,6 +31,7 @@ export const ALIAS = [
       { desc: 'get', example: 'Get alias details; help command [s alias get -h]' },
       { desc: 'list', example: 'View the list of alias; help command [s alias list -h]' },
       { desc: 'publish', example: 'Publish alias; help command [s alias publish -h]' },
+      { desc: 'rollback', example: 'Rollback alias versionId; help command [s alias rollback -h]' },
     ],
   },
 ];
@@ -155,6 +156,66 @@ export const ALIAS_PUBLISH = [
     header: 'Examples with CLI',
     content: [
       '$ s cli fc alias publish --region cn-hangzhou --service-name serviceName --alias-name aliasName --version-id 2',
+    ],
+  },
+];
+
+export const ALIAS_ROLLBACK = [
+  {
+    header: 'Alias rollback',
+    content: 'Rollback service alias',
+  },
+  {
+    header: 'Document',
+    content: 'https://github.com/devsapp/fc/blob/main/docs/zh/command/alias.md',
+  },
+  {
+    header: 'Usage',
+    content: '$ s alias rollback <options>',
+  },
+  {
+    header: 'Options',
+    optionList: [
+      regionDescribe,
+      serviceNameDescribe,
+      aliasNameDescribe,
+      {
+        name: 'version-id',
+        description: "[Required] The version Id. You can also use the keyword 'HEAD'",
+        type: String,
+      },
+      {
+        name: 'description',
+        description: '[Optional] Specify the alias description',
+        type: String,
+      },
+      {
+        name: 'gversion',
+        description: '[Optional] The grayscale version id',
+        type: String,
+      },
+      {
+        name: 'weight',
+        description: '[Optional] The weight for grayscale version',
+        type: String,
+      },
+    ],
+  },
+  { ...globalParams },
+  { ...globalDescribe },
+  {
+    header: 'Examples with Yaml',
+    content: [
+      '$ s alias rollback --alias-name aliasName --version-id 2',
+      '$ s alias rollback --alias-name aliasName --version-id HEAD^^',
+      '$ s alias rollback --alias-name aliasName --version-id HEAD~2',
+      '$ s alias rollback --description description --alias-name aliasName --version-id 2 --gversion 3 --weight 20',
+    ],
+  },
+  {
+    header: 'Examples with CLI',
+    content: [
+      '$ s cli fc alias rollback --region cn-hangzhou --service-name serviceName --alias-name aliasName --version-id HEAD^',
     ],
   },
 ];
